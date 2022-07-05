9c7275baafa394d5446112a40965f2599bec2573
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-07-05 13:34:49 +0700

    Update sewa.js

--- plugins/sewa.js
+++ plugins/sewa.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn }) => {
   let fetch = require('node-fetch')
-  let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/1575c60b4e1663bae8c86.jpg")
   let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
   let c = pickRandom([a, b])
   let d = await fetch(c).then(a => a.buffer())
   let prepare = await require('@adiwajshing/baileys').generateWAMessageFromContent(m.key.remoteJid,{listMessage:{
   title: `${await conn.getName(conn.user.jid)}`,
-  description: ` *• SEWA BOT & UP TO PREMIUM •*
+  description: ` *ღ SEWA BOT & UP TO PREMIUM ღ*
         
 karena bot baru di buat jadi untuk join gc free :)
 Lalu untuk premium chat own aja
@@ -21,7 +21,7 @@ Lalu untuk premium chat own aja
   jpegThumbnail: d },
   businessOwnerJid: `${owner[0]}@s.whatsapp.net`
   },
-  footerText: 'https://github.com/AhmdLui',
+  footerText: 'https://github.com/DanzGantevk',
   }},{})
   conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
 }
