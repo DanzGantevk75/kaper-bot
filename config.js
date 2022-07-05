8e56fb1e36883eb134052a68ad23126482f95765
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-07-05 13:10:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some h
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['6282146092695', 'AhmdLui', 'AhmdLui', 'ayonimasangat@gmail.com', true],
-['6289655134392', 'Rika', 'Rika❤️', 'Nothing!', true]
+['6288270863279', 'Danzz', 'Danzz', 'danzkontol@gmail.com', true],
+['6288270863279', 'Danzz', 'Danzz', 'Nothing!', true]
 
 ]
 global.APIs = { // API Prefix
@@ -37,23 +37,23 @@ global.APIKeys = { // APIKey Here
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 Kami-sama'
-global.linkgc = 'https://chat.whatsapp.com/CnQV9lNcHuIENb8SQ9CaYP'
-global.linkig = 'https://www.instagram.com/ahmdlui'
+global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 Danzz'
+global.linkgc = 'https://chat.whatsapp.com/EmCBNEjwdOcHzRlGCXKoxK'
+global.linkig = 'https://www.instagram.com/danzgamtenk'
 global.deslink = ''
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@ahmdlui'
+global.bodlink = '@Danzz'
 
 global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
 global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
 
 //========Url Template Buttons==========//
 global.dtu = '𝑭𝒐𝒍𝒍𝒐𝒘'
-global.urlnya = "https://www.instagram.com/ahmdlui"
+global.urlnya = "https://www.instagram.com/DanzGamtenk"
 
 //============= callButtons ============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-3362-82144'
+global.phn = '+62 882-7086-3279'
 
 //========== Tampilan Bot =============//
 global.sa = '╭─'
@@ -67,11 +67,11 @@ global.zt = '*'
 global.zc = ''
 
 global.thumbnailUrl = [
-  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
-  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
-  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
-  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
-  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
+  'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg', 'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg',
+  'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg', 'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg',
+  'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg', 'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg',
+  'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg', 'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg',
+  'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg', 'https://telegra.ph/file/1575c60b4e1663bae8c86.jpg'
 ]
 
 //============= Games ================//
@@ -87,7 +87,7 @@ global.baileys = require('@adiwajshing/baileys')
 global.fs = require('fs')
 global.data = JSON.parse(fs.readFileSync('./data.json'))
 global.fetch = require('node-fetch')
-global.bochil = require('@bochilteam/scraper')
+global.bochil = require('@bochilteam/DanzBot')
 
 global.rpg = {
   emoticon(string) {
